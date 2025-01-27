b041374d559804587e4bbc319999448b3608f239
Joanderson Coelho Macedo <joanderson.k10@gmail.com>
2025-01-27 20:46:14 -0300

    SelecaoRepository

--- API rest/src/app/repositories/SelecaoRepository.js
+++ API rest/src/app/repositories/SelecaoRepository.js
@@ -1,14 +1,66 @@
+import conexao from "../dataBase/conexao.js";
+
 class SelecaoRepository {
     //CRUD
-    create() {}
+    create(selecao) {
+        const sql = 'INSERT INTO selecoes SET ?;';
+        return new Promise((resouve, reject) => {
+            conexao.query(sql, selecao, (erro, resultado) => {
+                if(erro) return reject('Não foi possível localizar!');
+                
+                const row = JSON.parse(JSON.stringify(resultado));
+                return resouve(row);
+            });
+        });
+    }
 
-    selectAll() {}
+    selectAll() {
+        const sql = 'SELECT * FROM selecoes;';
+        return new Promise((resouve, reject) => {
+            conexao.query(sql, (erro, resultado) => {
+                if(erro) return reject('Não foi possível localizar!');
+                
+                const row = JSON.parse(JSON.stringify(resultado));
+                return resouve(row);
+            });
+        });
+    }
 
-    selectById() {}
+    selectById(id) {
+        const sql = 'SELECT * FROM selecoes WHERE id=?;';
+        return new Promise((resouve, reject) => {
+            conexao.query(sql, id, (erro, resultado) => {
+                if(erro) return reject('Não foi possível localizar!');
+                
+                const row = JSON.parse(JSON.stringify(resultado));
+                return resouve(row);
+            });
+        });
+    }
 
-    update() {}
+    update(selecao, id) {
+        const sql = 'UPDATE selecoes SET ? WHERE id=?;';
+        return new Promise((resouve, reject) => {
+            conexao.query(sql, [selecao, id], (erro, resultado) => {
+                if(erro) return reject('Não foi possível localizar!');
+                
+                const row = JSON.parse(JSON.stringify(resultado));
+                return resouve(row);
+            });
+        });
+    }
 
-    delete() {}
+    delete(id) {
+        const sql = 'DELETE FROM selecoes WHERE id=?;';
+        return new Promise((resouve, reject) => {
+            conexao.query(sql, id, (erro, resultado) => {
+                if(erro) return reject('Não foi possível localizar!');
+                
+                const row = JSON.parse(JSON.stringify(resultado));
+                return resouve(row);
+            });
+        });
+    }
 
 }
 
